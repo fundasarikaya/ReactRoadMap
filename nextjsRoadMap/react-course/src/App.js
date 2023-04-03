225833a8e5c070c72eaa1166cd7be913e8d093ba
fundasarikaya <fundasarikaya58@gmail.com>
2023-04-04 00:19:00 +0300

    Passing Data With Props & Dynamic Content

--- nextjsRoadMap/react-course/src/App.js
+++ nextjsRoadMap/react-course/src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Learn React" />
+      <Todo text="Mater React" />
+      <Todo text="Explore the full React course" />
     </div>
   );
 }
