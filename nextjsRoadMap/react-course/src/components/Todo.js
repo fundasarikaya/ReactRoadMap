e27b920682a219bfb7dd1ce8f3dd3d65ab8712a4
fundasarikaya <fundasarikaya58@gmail.com>
2023-04-09 02:09:59 +0300

    Event Props

--- nextjsRoadMap/react-course/src/components/Todo.js
+++ nextjsRoadMap/react-course/src/components/Todo.js
@@ -9,6 +9,11 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+  // Props are important for building reusable components. state is important for changing what we see on the screen dynamically
+  //for adding or removing elements for changing text on the screen or like in this case for showing and hiding an overlay
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -18,8 +23,10 @@ function Todo(props) {
         </button>
       </div>
       {/* {modalIsOpen ? <Modal/> : null}  */}
-      {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
