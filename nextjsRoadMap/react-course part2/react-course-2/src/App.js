3c1e928a7a5fbf06783d78f3a7954cffacf7f1ec
fundasarikaya <fundasarikaya58@gmail.com>
2023-04-11 00:50:42 +0300

    Created routing an navigation

--- nextjsRoadMap/react-course part2/react-course-2/src/App.js
+++ nextjsRoadMap/react-course part2/react-course-2/src/App.js
@@ -1,5 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigaiton";
+
 function App() {
-  return <div></div>;
+  //localhost:3000/page
+
+  return (
+    <div>
+      <MainNavigation />
+
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
